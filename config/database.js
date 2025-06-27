3eb057219cc3d1332b2a1fe7f5036e75eb079564
UpkarSingh583 <upkarsinghshb@gmail.com>
2025-06-28 02:19:52 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ const connectDB = async () => {
       bufferCommands: false, // Disable mongoose buffering
     };
 
-    const conn = await mongoose.connect(process.env.MONGODB_URI, options);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error.message);
